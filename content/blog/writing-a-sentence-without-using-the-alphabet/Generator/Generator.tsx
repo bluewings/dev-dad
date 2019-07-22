e5284a7594ab87a14319d4ee521f36fe0b0793fa
sungwon.cha <mygenie3@naver.com>
2019-07-22 13:18:49 +0900

    fix

--- content/blog/writing-a-sentence-without-using-the-alphabet/Generator/Generator.tsx
+++ content/blog/writing-a-sentence-without-using-the-alphabet/Generator/Generator.tsx
@@ -1,14 +1,47 @@
 import * as React from 'react';
-import { useCallback, useEffect, useMemo, useState, SyntheticEvent } from 'react';
+import { useCallback, useEffect, useMemo, useState, useRef, SyntheticEvent } from 'react';
 import { textToCode } from './util';
+import { Code } from '../../../../src/components/Code';
+import Skipped from './Skipped';
 import styles from './Generator.module.scss';
 
+const pickOne = (items: any) => items[~~(Math.random() * items.length)];
+
+const samples = [
+  'toy story 3',
+  'to inifinity and beyond',
+  'success',
+  'fail',
+  'bts',
+  'seoul',
+  'sydney',
+  'california',
+  'san francisco',
+  'aladdin',
+  'incredible',
+  'yoda',
+  'r2d2',
+  'octocat',
+  'nodejs',
+  'babel',
+  'starcourt',
+].sort();
+
 function Generator({ onChange, placeholder, lang }: any) {
-  const [text, setText] = useState('toy story 3');
+  const inputRef = useRef<any>();
+  const [text, setText] = useState(() => pickOne(samples));
+
   const handleTextChange = (event: SyntheticEvent) => {
     // @ts-ignore
     const userInput = event.target.value.trim();
     setText(userInput);
+  };
+  const handleSampleClick = (event: SyntheticEvent) => {
+    event.preventDefault();
+    // @ts-ignore
+    const userInput = event.target.innerText.trim();
+    setText(userInput);
+    inputRef.current.value = userInput;
   };
 
   const [newLine, setNewLine] = useState(true);
@@ -46,6 +79,7 @@ function Generator({ onChange, placeholder, lang }: any) {
     <div className={styles.root}>
       <div className={styles.input}>
         <input
+          ref={inputRef}
           type="text"
           defaultValue={text}
           placeholder={placeholder || ''}
@@ -53,6 +87,24 @@ function Generator({ onChange, placeholder, lang }: any) {
           autoComplete="off"
           spellCheck={false}
         />
+        <div className={styles.samples}>
+          {samples.map((e, i, arr) => {
+            return (
+              <>
+                <code>
+                  {e === text ? (
+                    e
+                  ) : (
+                    <a href="" onClick={handleSampleClick}>
+                      {e}
+                    </a>
+                  )}
+                </code>
+                {i < arr.length - 1 && ' , '}
+              </>
+            );
+          })}
+        </div>
         <label>
           <input type="checkbox" defaultChecked={newLine} onClick={handleNewLineToggle} />
           &nbsp;{lang === 'en' ? 'new line' : '줄바꿈'}
@@ -66,6 +118,14 @@ function Generator({ onChange, placeholder, lang }: any) {
           &nbsp;{lang === 'en' ? 'remove space' : '설명 표시'}
         </label>
       </div>
+      <Code codeString={code || ''} language="javascript" lineWrap={!newLine} clipboard={true} />
+      <p>
+        {lang === 'en'
+          ? 'Only numbers and alphabets are converted, but the following alphabets are not converted.'
+          : '숫자 · 영문자 이외의 문자, 그리고 다음 알파벳은 변환되지 않습니다.'}
+        <br />
+        <Skipped skipped={skipped} />
+      </p>
     </div>
   );
 }
